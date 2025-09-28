9c8ccd899928072513bc415255776d6f6e7598d3
tomyudev <tomyudev@gmail.com>
2025-09-28 16:40:41 -0500

    fixed typo header

--- components/ui/UserDropdown.tsx
+++ components/ui/UserDropdown.tsx
@@ -28,7 +28,7 @@ const UserDropdown = () => {
       <DropdownMenuTrigger asChild>
         <Button
           variant="ghost"
-          className="flex items-center gap-3 text-gray-4 hover:text-yellow-500"
+          className="flex items-center gap-3 text-gray-400 hover:text-yellow-500"
         >
           <Avatar className="h-8 w-8">
             <AvatarImage src="https://avatars.githubusercontent.com/u/153423955?s=280&v=4" />
